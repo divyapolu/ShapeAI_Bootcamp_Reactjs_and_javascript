4dede17cdf7e206cc043aec540e59990c348c5a0
divyapolu <71419245+divyapolu@users.noreply.github.com>
2021-06-05 13:54:56 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        {" "}
+        This is the best bootcamp I have learnt lot of things very clearly.
+        Every session is explaned from the scratch by Shourya Sinha sir.Very
+        happy to be part of ShapeAI rect.js and javascript bootcamp. Waiting for
+        more bootcamps!!!!
+      </p>
+    </div>
+  );
+}
+export default Note;
---
+++ index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+ document.getElementById("root"),
+);
